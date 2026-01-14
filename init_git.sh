3bd74d7914950e345f44dd6e6262820ba0fe53e2
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-01-14 10:37:39 +0000

    with git commands and defaut

---
+++ init_git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Sourcing default init scripts from InseeFrLab/sspcloud-init-scripts
+SCRIPT1_URL="https://raw.githubusercontent.com/InseeFrLab/sspcloud-init-scripts/refs/heads/main/vscode/customize-settings.sh"
+
+#
+# SCRIPT1_URL=$1
+
+# Download scripts
+curl -s -o "customize-settings.sh" "$SCRIPT1_URL"
+
+# Source the scripts
+source "customize-settings.sh"
+
+# Personnal init commands
+# To use git clone with automated PAT
+git config --global url."https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/".insteadOf "https://github.com/"
+
+# To avoid having to set the branch after having created it
+git config --global push.autoSetupRemote true
+
