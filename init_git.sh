e6f01e36df136557d0a2581a842b2297c2a603dc
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-01-14 10:55:26 +0000

    with vs code keybindings

--- init_git.sh
+++ init_git.sh
@@ -21,3 +21,58 @@ git config --global url."https://$GIT_PERSONAL_ACCESS_TOKEN@github.com/".instead
 # To avoid having to set the branch after having created it
 git config --global push.autoSetupRemote true
 
+# VS Code
+# Define the configuration directory for VS Code
+VSCODE_CONFIG_DIR="$HOME/.local/share/code-server/User"
+
+# Create the configuration directory if necessary
+mkdir -p "$VSCODE_CONFIG_DIR"
+
+# User settings file
+SETTINGS_FILE="$VSCODE_CONFIG_DIR/settings.json"
+
+# Enable dark mode by default
+echo '{
+    "workbench.colorTheme": "Default Dark Modern"
+}' > "$SETTINGS_FILE"
+
+
+# Keybindings file
+KEYBINDINGS_FILE="$VSCODE_CONFIG_DIR/keybindings.json"
+
+# Add shortcuts for duplicating, deleting lines, and navigating tabs
+echo '[
+    {
+        "key": "ctrl+shift+d",
+        "command": "editor.action.duplicateSelection"
+    },
+    {
+        "key": "ctrl+d",
+        "command": "editor.action.deleteLines",
+        "when": "editorTextFocus"
+    },
+    {
+        "key": "ctrl+down",
+        "command": "workbench.action.previousEditor"
+    },
+    {
+        "key": "ctrl+up",
+        "command": "workbench.action.nextEditor"
+    },
+    {
+        "key": "ctrl+numpad2",
+        "command": "workbench.action.terminal.focus"
+    },
+    {
+        "key": "ctrl+alt+down",
+        "command": "workbench.action.terminal.focusNext"
+    },
+    {
+        "key": "ctrl+numpad8",
+        "command": "workbench.action.focusActiveEditorGroup"
+    },
+    {
+        "key": "ctrl+numpad4",
+        "command": "workbench.files.action.focusFilesExplorer"
+    }
+]' > "$KEYBINDINGS_FILE"
